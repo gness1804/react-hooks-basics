f9dab4915b4b27039f7e60cb0f191bc9a0e2e851
gness1804 <graham.nessler@gmail.com>
2019-03-01 19:26:38 -0600

    Build basic counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0,
+    };
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1,
+    })
+  }
+
   render() {
+    const { count } = this.state;
+
     return (
         <>
-          Hello
+          <button onClick={this.incrementCount}>Click Here</button>
+          <p>I was clicked {count} times.</p>
         </>
     );
   }
