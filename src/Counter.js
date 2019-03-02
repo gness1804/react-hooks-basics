4ea7158877ea767f2f5d6feb8ffa0357cd69eafb
gness1804 <graham.nessler@gmail.com>
2019-03-01 20:25:56 -0600

    Add flashlight toggle

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
+import styles from "./styles";
 
 const Counter = () => {
   const [count, setCount] = useState(0);
+  const [isOn, setIsOn] = useState(false);
 
   const incrementCount = () => {
       setCount(prevCount => prevCount + 1);
   }
 
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn);
+  };
+
   return (
     <>
+      <h2>Counter</h2>
       <button onClick={incrementCount}>Click Me</button>
       <p>I was clicked {count} times.</p>
+
+      <h2>Toggle Light</h2>
+      <img
+          onClick={toggleLight}
+          style={styles.Counter.lightbulbImage}
+          alt="Flashlight"
+          src={isOn ? 'https://icon.now.sh/highlight/fd0' : 'https://icon.now.sh/highlight/aaa'}
+      />
     </>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,10 @@
+const styles = {
+  Counter: {
+    lightbulbImage: {
+      height: '50px',
+      width: '50px',
+    },
+  },
+};
+
+export default styles;
