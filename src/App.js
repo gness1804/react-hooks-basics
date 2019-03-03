3ce4585693989fc786f91d9054d7b6395ad61322
gness1804 <graham.nessler@gmail.com>
2019-03-02 19:32:15 -0600

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './Login';
-// import Counter from './Counter';
+import Counter from './Counter';
+import Register from './Register';
 
 class App extends Component {
   render() {
     return (
       <>
-        {/* <Counter /> */}
-        <Login />
+        <Router>
+          <>
+            <Route exact path="/" component={Login} />
+            <Route path="/counter" component={Counter} />
+            <Route path="/register" component={Register} />
+          </>
+        </Router>
       </>
     );
   }
